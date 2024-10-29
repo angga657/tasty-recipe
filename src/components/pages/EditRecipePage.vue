e7611e9d5fd62a31974ce895feffb8c6d6dc7378
angga657 <117795076+angga657@users.noreply.github.com>
2024-10-29 14:53:44 +0800

    initial commit

---
+++ src/components/pages/EditRecipePage.vue
@@ -0,0 +1,40 @@
+<template>
+    <main>
+        <div class="container-md my-5 py-5">
+            <recipe-form
+                v-if="detailData && !isLoading"
+                :isEdit="true" 
+                :detailData="detailData">
+            </recipe-form>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import RecipeForm from '../recipeForm/RecipeForm.vue';
+import { useStore } from 'vuex';
+import { useRoute } from 'vue-router';
+import { ref, onMounted } from 'vue';
+
+const store = useStore();
+const route = useRoute();
+const detailData = ref();
+const isLoading = ref(false);
+
+onMounted(async () => {
+    isLoading.value = true;
+    try {
+        const recipeId = route.params.id;
+        await store.dispatch("recipe/getRecipeDetail", recipeId);
+
+        detailData.value = store.state.recipe.recipeDetail;
+        console.log(detailData.value);
+        console.log(store.state);
+        isLoading.value = false;
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+
+</script>
---
+++ src/components/recipeForm/RecipeForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="list-group">
+        <recipe-form-header></recipe-form-header>
+        <recipe-form-body :isEdit="isEdit" :detailData="detailData"></recipe-form-body>
+    </ul>
+</template>
+
+<script setup>
+import RecipeFormHeader from "./RecipeFormHeader.vue"
+import RecipeFormBody from "./RecipeFormBody.vue"
+
+defineProps({
+    isEdit:{ type: Boolean, default: FileSystemWritableFileStream},
+        detailData: { type: Object, default: () => ({}) }
+})
+</script>
